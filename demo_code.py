24fcf81738be82f97bfd9ae5f5ae067afa891963
Eshaan Bansal <101061994+eshaan-deepsource@users.noreply.github.com>
2023-01-19 15:48:58 +0530

    Update demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -12,6 +12,7 @@ class BaseNumberGenerator:
 
     def __init__(self):
         self.limits = (1, 10)
+        self.xyz = "abc"
 
     def get_number(self, min_max):
         raise NotImplementedError

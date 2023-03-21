18d6719dac80899170b6ff7f87fb6cfef3cff412
Eshaan Bansal <eshaan@deepsource.io>
2023-03-21 15:12:36 +0530

    Update django_issues.py

--- django_issues.py
+++ django_issues.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 
 
+
 @require_http_methods(["GET", "POST"])  # Sensitive
 def current_datetime(request):
     now = datetime.datetime.now()

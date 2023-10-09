21c10270e965012ac6ab3b53bc506458ea3469dd
Eshaan Bansal <eshaan@deepsource.io>
2023-10-09 19:26:20 +0530

    Update demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -131,7 +131,7 @@ def wrong_callable():
 
 if __name__ == "__main__":
     args = ["--disable", "all"]
-    for arg in args:
-        has_truthy = bool(arg)
+    for i in range(len(args)):
+        has_truthy = bool(args[i])
         if has_truthy:
             break

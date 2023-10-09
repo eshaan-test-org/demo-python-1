4f37229217990e4f83257d618dcd2b0ab1979b1c
Eshaan Bansal <eshaan@deepsource.io>
2023-10-09 17:26:27 +0530

    test-pr-to-master

--- assignment.py
+++ assignment.py
@@ -1,8 +1,16 @@
 (*FIRST,) = [1, 2, 3]  # documenting code no fun
 *FIRST, a, b = [1, 2, 3]  # documenting code no fun
 a, b = [1, 2]  # documenting code no fun
+c = 1
 
 def not_covered():
-  """
-  """
-  pass
+	"""
+ 	empty
+ 	"""
+	pass
+
+def bar():
+	"""
+ 	empty
+        """
+	pass

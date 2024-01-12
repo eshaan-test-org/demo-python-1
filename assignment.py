5116bc712c24d89e24bc84d684510cc402099997
Eshaan Bansal <eshaan@deepsource.io>
2024-01-12 16:54:15 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,3 +2,13 @@
 *FIRST, a, b = [1, 2, 3]  # documenting code no fun
 a, b = [1, 2]  # documenting code no fun
 
+def foo():
+  """
+  """
+  pass
+
+def bar():
+  items = []
+  for i in range(2):
+    items.append(i)
+  return items
